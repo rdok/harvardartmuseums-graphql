3adc362891ec5b5c3be65de4090778361002f847
Rizart Dokollari <r.dokollari@gmail.com>
2022-04-27 07:30:17 +0100

    Revert "Remove secrets manager due to time constraints."
    This reverts commit 43635bea132c0f63c57323cf3fc0a4e10dc14cc8.

--- cicd-authorisation/lib/cicd-authorisation-stack.ts
+++ cicd-authorisation/lib/cicd-authorisation-stack.ts
@@ -14,6 +14,7 @@ import { DescribeCloudformationStacks } from "./describe-cloudformation-stacks";
 import { ApiGatewayPolicy } from "./api-gateway-policy";
 import { DomainPolicy } from "./domain-policy";
 import { MonitorPolicy } from "./monitor-policy";
+import { SecretsManagerPolicy } from "./secrets-manager-policy";
 
 export class CICDAuthorisationStack extends Stack {
   constructor(
@@ -56,5 +57,6 @@ export class CICDAuthorisationStack extends Stack {
     new ApiGatewayPolicy(this, { config, stackRegex, role });
     new DomainPolicy(this, { config, stackRegex, role });
     new MonitorPolicy(this, { config, stackRegex, role });
+    new SecretsManagerPolicy(this, { config, stackRegex, role });
   }
 }
